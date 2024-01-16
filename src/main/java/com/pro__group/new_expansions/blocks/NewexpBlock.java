e82e08b4313839de7d0fea851c178c4ae998a901
Pro__Dan <90392478+ProDanJanurz@users.noreply.github.com>
2024-01-16 04:32:11 +0100

    Delete src/main directory

--- src/main/java/com/pro__group/new_expansions/blocks/NewexpBlock.java
+++
@@ -1,9 +0,0 @@
-package com.pro__group.new_expansions.blocks;
-
-import net.minecraft.block.Block;
-
-public class NewexpBlock extends Block {
-    public NewexpBlock(Settings settings) {
-        super(settings);
-    }
-}
--- src/main/java/com/pro__group/new_expansions/items/NewexpItem.java
+++
@@ -1,9 +0,0 @@
-package com.pro__group.new_expansions.items;
-
-import net.minecraft.item.Item;
-
-public class NewexpItem extends Item {
-    public NewexpItem(Settings settings) {
-        super(settings);
-    }
-}
